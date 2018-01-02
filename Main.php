fa5551b8f0bbb6cfc7a8f15218c6015dc5bf1bff
GplCart <gplcart.software@gmail.com>
2018-01-02 16:00:18 +0200

    Rename main module class

--- Main.php
+++ Main.php
@@ -9,12 +9,12 @@
 
 namespace gplcart\modules\autocomplete_search;
 
-use gplcart\core\Module as CoreModule;
+use gplcart\core\Module;
 
 /**
  * Main class for Autocomplete search module
  */
-class Module
+class Main
 {
 
     /**
@@ -24,9 +24,9 @@ class Module
     protected $module;
 
     /**
-     * @param CoreModule $module
+     * @param Module $module
      */
-    public function __construct(CoreModule $module)
+    public function __construct(Module $module)
     {
         $this->module = $module;
     }
